d7c76b34dc4f89df99a61af82b3bdd64aad540fe
Robert <robert.schoefbeck@cern.ch>
2015-07-29 21:14:56 +0200

    746patch6 JetHT rereco with M0

---
+++ dataRereco_746patch6/RECO.py
@@ -0,0 +1,71 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step3 --conditions auto:run1_data_Fake --scenario pp --process reRECO --data --eventcontent RECO -s RAW2DIGI,RECO --datatier RECO --python RECO.py --conditions 74X_dataRun2_Prompt_v1 -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('reRECO')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('root://eoscms.cern.ch//store/data/Run2015B/JetHT/RAW/v1/000/251/161/00000/3686240A-0925-E511-A01E-02163E0137CC.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step3 nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string('step3_RAW2DIGI_RECO.root'),
+    outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_Prompt_v1', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.reconstruction_step,process.endjob_step,process.RECOoutput_step)
+
+
---
+++ dataRereco_746patch6/crab.py
@@ -0,0 +1,34 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+#from optparse import OptionParser
+#parser = OptionParser()
+#parser.add_option("--cmd", dest="command", default='status', type="string", action="store", help="What to do.")
+#(options, args) = parser.parse_args()
+
+
+config.section_("General")
+config.General.requestName = "JetHT_Run2015B-v1_74X_dataRun2_Prompt_v1_M0"
+config.General.workArea = "reReco"
+config.General.transferOutputs = True #whether to transfer
+config.General.transferLogs = False #1MB still available
+#config.General.failureLimit =  #0.1 or 10% (which?) fraction of tolerated failures
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName   = 'RECO.py'
+#config.JobType.inputFiles = ['PFCalibration.db']
+#config.JobType.allowNonProductionCMSSW = True
+#config.JobType.pyCfgParams   = [ 'keep=*_genMetTrue_*_*,*_pfMet_*_*,*_packedPFCandidates_*_*,*_prunedGenParticles_*_*,*_packedGenParticles_*_*', 'GT=POSTLS170_V6::All']
+config.section_("Data")
+config.Data.lumiMask = 'Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt'
+config.Data.publishDBS = 'phys03'
+config.Data.inputDataset   = '/JetHT/Run2015B-v1/RAW'
+config.Data.splitting   = 'LumiBased'
+config.Data.unitsPerJob = 2
+config.Data.publication = True
+#config.Data.publishDataName = 'mAOD730p1'
+#config.Data.totalUnits = 
+config.section_("Site")
+config.Site.storageSite = 'T2_AT_Vienna'
+
