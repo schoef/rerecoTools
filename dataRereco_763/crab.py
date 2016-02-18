c943d26d91ba1a1bb4293d00fd8c198f153f5e77
schoefbeck <robert.schoefbeck@cern.ch>
2016-02-18 10:50:17 +0100

    data rereco 763

---
+++ dataRereco_763/crab.py
@@ -0,0 +1,27 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "JetHT_Run2015D_M2_5_500"
+config.General.workArea = "dataRereco"
+config.General.transferOutputs = True #whether to transfer
+#config.General.failureLimit =  #0.1 or 10% (which?) fraction of tolerated failures
+config.General.transferLogs = True
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName   = 'reco_Run2015D_JetHT_M2_5_500.py'
+config.JobType.maxMemoryMB = 2300
+#config.JobType.allowNonProductionCMSSW = True
+#config.JobType.pyCfgParams   = [ 'keep=*_genMetTrue_*_*,*_pfMet_*_*,*_packedPFCandidates_*_*,*_prunedGenParticles_*_*,*_packedGenParticles_*_*', 'GT=POSTLS170_V6::All']
+config.section_("Data")
+config.Data.publishDBS = 'phys03'
+config.Data.lumiMask = 'json_260431.txt'
+#config.Data.inputDBS = 'phys03'
+config.Data.inputDataset   = '/JetHT/Run2015D-v1/RAW'
+config.Data.splitting   = 'LumiBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = True
+#config.Data.publishDataName = 'mAOD730p1'
+#config.Data.totalUnits = 1000000
+config.section_("Site")
+config.Site.storageSite = 'T2_AT_Vienna'
