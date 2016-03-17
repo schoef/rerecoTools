308db5210c1a3ced57d00a1e819f90c9f85f6de7
schoefbeck <robert.schoefbeck@cern.ch>
2016-03-17 10:10:31 +0100

    mAOD 763

--- miniAOD_763/crab_mAOD.py
+++ miniAOD_763/crab_mAOD.py
@@ -11,7 +11,10 @@ config.section_("General")
 #config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8"
 #config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_M0"
 #config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_M21p"
-config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_M23p"
+#config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_M23p"
+#config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_M2_0_500"
+config.General.requestName = "QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_M2_5_100"
+
 config.General.workArea = "mAOD"
 config.General.transferOutputs = True #whether to transfer
 config.General.transferLogs = True #1MB still available
@@ -30,8 +33,9 @@ config.Data.publishDBS = 'phys03'
 
 #config.Data.inputDataset = '/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/schoef-crab_QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIISummer15GS-MCRUN2_71_V1-v1_M0-4a211b02976d22a147c07c4d61c32aac/USER'
 #config.Data.inputDataset = '/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/schoef-crab_QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIISummer15GS-MCRUN2_71_V1-v1_M21p-5989d4c1503e8745d17ebfdf06e40d89/USER'
-config.Data.inputDataset = '/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/schoef-crab_QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIISummer15GS-MCRUN2_71_V1-v1_M23p-495340a8779130ae92e9241afb134c5c/USER'
-
+#config.Data.inputDataset = '/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/schoef-crab_QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIISummer15GS-MCRUN2_71_V1-v1_M23p-495340a8779130ae92e9241afb134c5c/USER'
+#config.Data.inputDataset = '/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/schoef-crab_QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIISummer15GS-MCRUN2_71_V1-v1_M2_0_500-d5b1a10d31b95b772f523bd125ea693a/USER'
+config.Data.inputDataset = '/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/schoef-crab_QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8_RunIISummer15GS-MCRUN2_71_V1-v1_M2_5_100-6c1deadea35cd533b1aba1b4f943152d/USER'
 
 config.Data.splitting   = 'FileBased'
 config.Data.unitsPerJob = 10
